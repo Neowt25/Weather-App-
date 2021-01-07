f00e6c7030bfff88610c64ba3abbe745fd72b6c4
Neo <neo.momo2@gmail.com>
2021-01-07 21:37:33 +0100

    Weather App using react to learn axios & request from apis

---
+++ src/Home.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import './Home.css'
+import axios from "axios"
+
+const Home = () => {
+    const [city, setCity] = useState('');
+    const [weather, setWeather] = useState({});
+
+    const options = {
+        method: 'GET',
+        url: 'https://community-open-weather-map.p.rapidapi.com/weather',
+        params: {
+            q: city,
+            units: '"metric" or "imperial"',
+        },
+        headers: {
+            'x-rapidapi-key': '15025d2992mshc08ad044b70e7fap10812ejsn7546112a70ef',
+            'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com'
+        }
+    };
+
+    const search = evt => {
+        if (evt.key === "Enter") {
+            axios.request(options).then(result => {
+                setCity('');
+                setWeather(result)
+                console.log(weather);
+            });
+
+        }
+    }
+    return (
+        <div className="home">
+            <div>
+                <div className="home__container">
+                    <input type="text" placeholder="Search..." onChange={e => setCity(e.target.value)} value={city} onKeyPress={search} />
+                </div>
+                {(typeof weather.data != "undefined") ? (
+                    <div className="home__result">
+                        <h1>{weather.data.name},{weather.data.sys.country}</h1>
+                        <h3>07 january 2021</h3>
+                        <h2>{Math.round(weather.data.main.temp - 273.15)}°C</h2>
+                    </div>
+                ) : (<div></div>)}
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
